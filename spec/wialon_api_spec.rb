84dc161b3aa6b44a6482209fabe2fb9861096f47
Arsen Shamkhalov <thornu731@gmail.com>
2015-02-06 22:34:22 +0300

    namespace fix

--- spec/wialon_api_spec.rb
+++ spec/wialon_api_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe WialonApi do
   it 'should return current version' do
-    expect(Wialon::VERSION).to eq('0.0.1')
+    expect(WialonApi::VERSION).to eq('0.0.1')
   end
 end
