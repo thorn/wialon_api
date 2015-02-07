bd828773780356780712e63b2ebd970858f3a7c5
Arsen Shamkhalov <thornu731@gmail.com>
2015-02-07 14:29:11 +0300

    added initializer generator for rails projeccts

---
+++ lib/generators/wialon_api/install/install_generator.rb
@@ -0,0 +1,10 @@
+# A rails generator `vkontakte_api:install`.
+# It creates a config file in `config/initializers/vkontakte_api.rb`.
+class WialonApi::InstallGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+
+  # Creates the config file.
+  def create_initializer
+    copy_file 'initializer.rb', 'config/initializers/wialon_api.rb'
+  end
+end
---
+++ lib/generators/wialon_api/install/templates/initializer.rb
@@ -0,0 +1,30 @@
+WialonApi.configure do |config|
+  # Faraday adapter to make requests with:
+  # config.adapter = :net_http
+
+  # Faraday connection options
+  # config.faraday_options = {}
+
+  # HTTP verb for API methods (:get or :post)
+  # config.http_verb = :post
+
+  # Number of retries when connection is failed
+  # config.max_retries = 1
+
+
+  # Logging parameters:
+  # log everything through the rails logger
+  config.logger = Rails.logger
+
+  # log requests' URLs
+  # config.log_requests = true
+
+  # log response JSON after errors
+  # config.log_errors = true
+
+  # log response JSON after successful responses
+  # config.log_responses = false
+
+  # Wialon server host
+  # config.wialon_host = 'https://hst-api.wialon.com/wialon/ajax.html'
+end
