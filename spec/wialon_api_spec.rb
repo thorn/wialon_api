3ce9492b76f8d68783f5e934f8f1cbf00c9197db
Arsen Shamkhalov <thornu731@gmail.com>
2015-02-17 13:32:16 +0300

    fixed test

--- spec/wialon_api_spec.rb
+++ spec/wialon_api_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe WialonApi do
   it 'should return current version' do
-    expect(WialonApi::VERSION).to eq('0.0.1')
+    expect(WialonApi::VERSION).to eq('0.0.2')
   end
 end
