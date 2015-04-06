b862386febc536f0911423e06e77b29d3ec70ab5
Arsen Shamkhalov <thornu731@gmail.com>
2015-04-06 20:11:36 +0300

    added wialon_edition config variable

--- lib/wialon_api/authorization.rb
+++ lib/wialon_api/authorization.rb
@@ -3,7 +3,11 @@ module WialonApi
     def authorize(user, password)
       response = WialonApi::Api.call('core/login', user: user, password: password)
       result = WialonApi::Result.process(response)
-      WialonApi::Client.new(result.ssid)
+      if WialonApi.wialon_edition == :pro
+        WialonApi::Client.new(result.ssid)
+      else
+        WialonApi::Client.new(result.eid)
+      end
     end
   end
 end
